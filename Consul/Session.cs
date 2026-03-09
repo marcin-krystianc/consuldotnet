b6ebc6fee3809abb9e9856544f1e19438cd38c6f
octocat <octocat@users.noreply.github.com>
2026-03-09 13:26:58 +0000

    style: fix whitespaces

--- Consul/Session.cs
+++ Consul/Session.cs
@@ -16,7 +16,7 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
-using System; 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
